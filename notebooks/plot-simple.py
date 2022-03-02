771b2ae5ccc034251f5d4896696200a2c8ef2f6a
Søren Winkel Holm <swholm@protonmail.com>
2022-03-02 21:30:26 +0100

    Prettify plot

--- notebooks/plot-simple.py
+++ notebooks/plot-simple.py
@@ -17,9 +17,12 @@ fig, ax = plt.subplots(figsize=figsize_std)
 ax.set_title("Scaling over clients sampled")
 ax.set_ylabel("Accuracy on 10K test images [%]")
 ax.set_xlabel("Number of clients sampled each round out of 10")
-ax.plot(clients, final_accuracies,  "o--", color=tab_colours[1], lw=3, ms=5, label="FedAvg with 500 obs. per device, 2 local epochs, 5 rounds")
-ax.axhline(97.77, lw=3, label="Centralized learning with 5000 obs, 10 epochs")
+ax.plot(clients, final_accuracies,  "o-", color=tab_colours[1], lw=2, ms=10, label="FedAvg with 500 obs. per device, 2 local epochs, 5 rounds")
+ax.axhline(97.77, ls="-", lw=2, label="Centralized learning with 5000 obs, 10 epochs")
 ax.set_ylim(93, 99)
 ax.legend()
 ax.grid()
+fig.savefig("reports/imgs/scaling.pdf")
 
+
+# %%
