9fc33b53c8f3d14b5d60317a46d5f5ae64744129
Asger Schultz <asger.s@protonmail.com>
2022-04-04 20:53:41 +0200

    Plot local accuracies

--- src/visualization/plot_federated.py
+++ src/visualization/plot_federated.py
@@ -13,39 +13,64 @@ from src.models.train_federated import Results
 update_rc_params(rc_params)
 
 
-def plot_timeline():
-    log.section("Plotting timeline")
+def plot_accuracy():
+    log.section("Plotting accuracy")
     res = Results.load()
+    train_cfg = res.cfg.configs.training
+
+    local_epochs = train_cfg.local_epochs
+    comm_rounds = train_cfg.communication_rounds
+    comm_rounds = np.arange(1, comm_rounds+1)
 
     plt.figure(figsize=figsize_std)
-    plt.title("Training on %i devices" % len(res.telemetry))
-    start_time = res.telemetry[0]["timestamp"][0]
-    log("Plotting accuracy")
+    for i in comm_rounds-1:
+        for j in range(res.clients_per_round):
+            if i:
+                x = i + np.arange(local_epochs+1) / local_epochs
+                y = [res.test_accuracies[i-1], *res.local_accs[i][j]]
+            else:
+                x = i+np.arange(1, local_epochs+1)/local_epochs
+                y = res.local_accs[i][j]
+            plt.plot(x, y, alpha=0.5, label="Local accuracy" if not (i or j) else None, color="gray")
+
     plt.plot(
-        np.array(res.eval_timestamps)-start_time,
+        comm_rounds,
         res.test_accuracies,
         "-o",
         label="Test accuracy",
     )
+    plt.title("Accuracy")
+    plt.xlabel("Communication rounds")
+    plt.ylabel("Accuracy [%]")
+    plt.ylim([-5, 105])
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig("plots/accuracy.png")
+    plt.close()
+
+def plot_memory():
+    log.section("Plotting memory")
+    res = Results.load()
+
+    plt.figure(figsize=figsize_std)
+    plt.title("Memory usage")
+    start_time = res.telemetry[0]["timestamp"][0]
     if res.ip:
-        log("Plotting memory usage")
-        for i, tel in enumerate(res.telemetry):
+        for tel in res.telemetry:
             plt.plot(
                 np.array(tel["timestamp"])-start_time,
                 tel["memory_usage"],
                 c="gray",
-                alpha=0.5,
-                label="Memory usage" if i == 0 else None,
+                alpha=0.7,
             )
 
     plt.xlabel("Time [s]")
-    plt.ylabel("%")
+    plt.ylabel("Total memory usage [%]")
     plt.ylim([-5, 105])
-    plt.legend(loc="lower right")
     plt.grid()
     plt.tight_layout()
-    plt.savefig("plots/timeline.png")
-
+    plt.savefig("plots/memory.png")
+    plt.close()
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -56,4 +81,5 @@ if __name__ == "__main__":
 
     log.configure("plots.log")
     with log.log_errors:
-        plot_timeline()
+        plot_accuracy()
+        plot_memory()
