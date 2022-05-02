69ef654b8f69f7ebc3d024505bba77cdc2e1df55
Søren Winkel Holm <swholm@protonmail.com>
2022-05-03 01:08:02 +0200

    Add final table

--- reports/table_multirun.py
+++ reports/table_multirun.py
@@ -9,55 +9,71 @@ from texttable import Texttable
 from latextable import draw_latex
 from src.models.train_federated import Results
 
-NAMES = dict(alpha=r"class balance ($\alpha$)", clients_per_round="clients sampled", noisy_clients="noisy clients", local_epochs="local epochs")
+NAMES = dict(
+    local_epochs="Local epochs ($E$)",
+    clients_per_round="Clients sampled ($S$)",
+    alpha=r"Class balance ($\alpha$)",
+    noisy_clients="Noisy clients ($N_K$)",
+)
+
 
 def results_and_cfgs(path: str):
     jobs = os.listdir(path)
     res = [Results.load(os.path.join(path, j)) for j in jobs if j.isdigit()]
     return res, [r.cfg.configs.training for r in res]
 
+
 def table_over_arg():
     M, S, V, U = list(), list(), list(), list()
-    for var in os.listdir():
+    for var in NAMES.keys():
         if var == "iid":
             continue
         results, configs = results_and_cfgs(var)
         values = np.array([cfg[var] for cfg in configs])
         if var == "alpha":
-            r2, c2 =  results_and_cfgs("iid")
+            r2, c2 = results_and_cfgs("iid")
             results.extend(r2)
             configs.extend(c2)
-            values = np.concatenate((values, ["iid"]*len(r2)))
+            values = np.concatenate((values, ["iid"] * len(r2)))
         perfs = np.array([r.test_accuracies[-1] for r in results])
         uniques = np.unique(values)
         M.append(np.array([np.mean(perfs[values == v]) for v in uniques]))
         S.append(np.array([np.std(perfs[values == v], ddof=1) for v in uniques]))
         V.append(var)
         U.append(uniques)
-    start, end =\
-r"""
+    start, end = (
+        r"""
 \begin{table}[htb!]
     \centering
-\resizebox{\linewidth}{!}{
-    \begin{tabular}""",\
-r"""\end{tabular}}
+    \begin{tabular}""",
+        r"""\end{tabular}
     \caption{
-    Test accuracy [\%] of final models using FedAvg over $K=40$ clients when running for $L=20$ local epochs.
-    Each run is repeated five times with an approximate 95\%\ confidence interval suggested.
+    Final test accuracies [\%] of FedAvg models over $K=40$ clients when running for $L=20$ communication rounds.
+    Each run is repeated five times to produce an approximate 95\%\ confidence interval.
     }
     \label{tab:main}
-\end{table}"""
+\end{table}""",
+    )
     r = max(x.size for x in M)
-    mc = lambda x : r"        \multicolumn{"+f"{r}"+r"}{c}{"+f"{x}"+r"}\\" + "\n"
+    mc = lambda x: r"        \multicolumn{" + f"{r}" + r"}{c}{" + f"{x}" + r"}\\" + "\n"
     s = start + "{" f"{'l'*r}" "}\n"
-    s += r"       \hline""\n"
+    s += r"       \hline" "\n"
     for means, stds, var, uniques in zip(M, S, V, U):
-        s += mc("Varying "+ NAMES[var])
-        s += "        "+ " & ".join(f"{x}" for x in uniques) + r" \\" + "\n"
-        s += r"       \hline""\n"
-        s += "        "+" & ".join(f"${m:.1f}"r" \pm " + f"{1.96 / len(stds)**0.5 * s:.1f}$" for m, s in zip(means, stds)) + r" \\" + "\n"
+        s += mc(NAMES[var])
+        s += "        " + " & ".join(f"{x}" for x in uniques) + r" \\" + "\n"
+        s += r"       \hline" "\n"
+        s += (
+            "        "
+            + " & ".join(
+                f"${m:.1f}" r" \pm " + f"{1.96 / len(stds)**0.5 * s:.1f}$"
+                for m, s in zip(means, stds)
+            )
+            + r" \\"
+            + "\n"
+        )
     s += end
     print(s)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
