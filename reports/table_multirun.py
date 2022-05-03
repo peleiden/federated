a0765444c6a164bf86079737ebba3535a1248f12
Søren Winkel Holm <swholm@protonmail.com>
2022-05-03 20:33:41 +0200

    Add Fed-DF results

--- reports/table_multirun.py
+++ reports/table_multirun.py
@@ -1,21 +1,18 @@
 from argparse import ArgumentParser
 import os
 
-import matplotlib.pyplot as plt
 import numpy as np
 
-
-from texttable import Texttable
-from latextable import draw_latex
 from src.models.train_federated import Results
 
 NAMES = dict(
     local_epochs="Local epochs ($E$)",
     clients_per_round="Clients sampled ($S$)",
     alpha=r"Class balance ($\alpha$)",
-    noisy_clients="Noisy clients ($N_K$)",
 )
-
+NAMES["alpha-df"] = r"FedDF: Class balance ($\alpha$)"
+NAMES["noisy_clients"] = "Noisy clients ($N_K$)"
+NAMES["noisy_clients-df"] = r"FedDF: Noisy clients ($N_K$)"
 
 def results_and_cfgs(path: str):
     jobs = os.listdir(path)
@@ -26,15 +23,15 @@ def results_and_cfgs(path: str):
 def table_over_arg():
     M, S, V, U = list(), list(), list(), list()
     for var in NAMES.keys():
-        if var == "iid":
+        if "iid" in var:
             continue
         results, configs = results_and_cfgs(var)
-        values = np.array([cfg[var] for cfg in configs])
-        if var == "alpha":
-            r2, c2 = results_and_cfgs("iid")
+        values = np.array([cfg[var.replace("-df", "")] for cfg in configs])
+        if "alpha" in var:
+            r2, c2 = results_and_cfgs("iid" if "df" not in var else "iid-df")
             results.extend(r2)
             configs.extend(c2)
-            values = np.concatenate((values, ["iid"] * len(r2)))
+            values = np.concatenate((values, ["iid."] * len(r2)))
         perfs = np.array([r.test_accuracies[-1] for r in results])
         uniques = np.unique(values)
         M.append(np.array([np.mean(perfs[values == v]) for v in uniques]))
@@ -48,7 +45,8 @@ def table_over_arg():
     \begin{tabular}""",
         r"""\end{tabular}
     \caption{
-    Final test accuracies [\%] of FedAvg models over $K=40$ clients when running for $L=20$ communication rounds.
+    Final test accuracies [\%] of FL models over $K=40$ clients when running for fixed $L=20$ communication rounds.
+    Where nothing else is stated, parameters correspond to Table \ref{tab:baseline}.
     Each run is repeated five times to produce an approximate 95\%\ confidence interval.
     }
     \label{tab:main}
